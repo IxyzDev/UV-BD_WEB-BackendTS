04bc98ed2dc4b40ebe40b36b68e3a2a3e46c2998
Kevin Diaz <kevin.diazlumnos.uv.cl>
2022-10-18 23:35:16 -0300

    Ordenamiento de las rutas

--- backend/app/models/index.ts
+++ backend/app/models/index.ts
@@ -1,10 +1,16 @@
-const fs = require('fs')
-const path = require('path')
-const Sequelize = require('sequelize')
+import * as fs from 'fs';
+import * as path from 'path';
+//const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
+
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../config/db.config.js')[env]
 const db: any = {}
+
+//import foo = require('foo');
+//require('foo');
+//import foo from 'foo';
 
 let sequelize: any
 
@@ -14,9 +20,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter((file: string) => {
+fs.readdirSync(__dirname).filter((file: string) => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts')
   })
   .forEach((file: any) => {
--- backend/app/routes/publicacion.routes.ts
+++ backend/app/routes/publicacion.routes.ts
@@ -1,11 +1,29 @@
-import express from 'express'
-
-// const publicacionControllers = require('../controllers/publicacionControllers')
-
-// import * as publicacionControllers from '../controllers/publicacionControllers';
+import express, { Request, Response } from 'express'
+import * as pc from '../controllers/publicacionController'
+import db from '../models'
 
 const router = express.Router()
 
-// router.get("/hello", publicacionControllers);
+router.get('/read', async (_req: Request, res: Response) => {
+  try {
+    const publicaciones = await pc.getEntries()
+    return res.json(publicaciones)
+  } catch (error) {
+    return res.json({ msg: 'Error al leer papito' })
+  }
+})
 
-export default router
+router.post('/create', async (req: Request, res: Response) => {
+  try {
+    const newPublicacionEntry = pc.postPublicacion({ ...req.body })
+
+    const record = db.Publicacion.create(newPublicacionEntry)
+    // res.json(newPublicacionEntry)
+    return res.json({ record, msg: 'Bien' })
+  } catch (error) {
+    console.log(error)
+    return res.json({ msg: 'Error al subir papito' })
+  }
+})
+
+module.exports = router;
