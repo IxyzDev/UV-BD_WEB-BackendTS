21f66b48bd941a5fe627f30d3b6b582fe6d44f53
ZarateBenjamin <benjamin.zarate@alumnos.uv.cl>
2022-11-06 13:14:55 -0300

    frontend 2 actualizado

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,3 +1,34 @@
+import React, { useState } from 'react';
+import NotesList from '../components/Part2/NotesList';
+import CreateNotes from '../components/Part2/CreateNotes';
+import { Note } from '../Models/note.model';
+import { Col, Container, Row } from 'react-bootstrap';
+
+
 export function About() {
-  return <h1>About</h1>
-}
+    const [notes, setNotes] = useState<Note[]>([{
+      id: (new Date()).toString(),
+      title: "Ale gay",
+      text: "Si, el ale es gay",
+      color: "#dfdfdf",
+      date: (new Date()).toString()
+    }]);
+  
+  
+    return (
+      <>
+        <Container className="mt-5">
+          <Row>
+            <Col>
+              <NotesList notes={notes} setNotes={ setNotes}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <CreateNotes notes={notes} setNotes={ setNotes}/>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
