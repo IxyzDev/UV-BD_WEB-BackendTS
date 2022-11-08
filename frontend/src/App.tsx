b42412b190fbcdd93ea0b0527bae97d58d8a7434
ZarateBenjamin <benjamin.zarate@alumnos.uv.cl>
2022-11-08 16:17:13 -0300

    fetch hecho cen axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,62 @@
-import { Routes, Route } from "react-router-dom"
-import { Container } from "react-bootstrap"
-import { Home } from "./pages/Home"
-import { Store } from "./pages/Store"
-import { About } from "./pages/About"
-import { Navbar } from "./components/Navbar"
-import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./styles.css";
 
-function App() {
+type resultProps = {
+  rutUsuario: string
+  nombreUsuario: string
+  correoUsuario: string
+  contrasenaUsuario: string
+  direccionUsuario: string
+};
 
+export default function App() {
+  const [result, setResult] = useState<resultProps[]>([]);
+
+  const api = async () => {
+      axios({
+        method: 'get',
+        url: 'http://localhost:3000/usuario/read',
+      })
+      .then(function (response:any) {
+        setResult(response?.data)
+      }).catch((err)=>{
+        console.log(err)
+      })
+
+      
+      // const apiResponse = await fetch("http://localhost:3000/usuario/read",{
+      //     method: 'GET',
+      //     headers: { 'Content-type': 'application/json'},
+      // });
+      // console.log(apiResponse)
+      // const json = await apiResponse.json();
+    // const jsonData = await data.json();
+    // setResult(jsonData.results);
+  };
+
+  useEffect(() => {
+    console.log("JASHDKAJHS")
+    api();
+  }, []);
+/*  rutUsuario: string
+  nombreUsuario: string
+  correoUsuario: string
+  contrasenaUsuario: string
+  direccionUsuario: string */
   return (
-    <ShoppingCartProvider>
-      <Navbar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
-  )
+    <div className="App">
+      <h1>
+        {result?.map((value: resultProps) => {
+          return (
+            <div>
+              <div>{value.rutUsuario}</div>
+              <div>{value.nombreUsuario}</div>
+            </div>
+          );
+        })}
+      </h1>
+      <h2>Json File from DB_Backend.</h2>
+    </div>
+  );
 }
-
-export default App
-
-
--- frontend/src/interfaces/note.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Note {
-    id: any,
-    title: string,
-    text: string,
-    color: string,
-    date: string
-  }
